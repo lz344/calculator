d72d86ee78da62b5872e20f787a5c59aff00c365
lz344 <luiz.adriano.lz.l@gmail.com>
2025-06-04 22:31:16 -0300

    Refatora funções da calculadora
    Os nomes das variáveis foram melhoras, a lógica do botão igual e do ponto
    decimal foram corrigidas.

--- assets/sricpt.js
+++ assets/sricpt.js
@@ -1,6 +1,5 @@
-function addOperatorOrNot(btnValue, currentValue) {
+function addOperatorOrNot(btnValue, currentValue, operators) {
   let lastIndex = currentValue.length - 1;
-  const operators = ["+", "-", "*", "/", "."];
 
   if (
     operators.includes(currentValue[lastIndex]) &&
@@ -11,35 +10,33 @@ function addOperatorOrNot(btnValue, currentValue) {
   return true;
 }
 
-function operatorInFront(btnValue, visor) {
-  const operators = ["+", "-", "*", "/", "."];
-
+function operatorInFront(btnValue, visor, operators) {
   if (operators.includes(btnValue) && visor === "") {
     return true;
   }
   return false;
 }
 
-function equalAfterNumber(visor) {
-  if (visor.length === 1) {
+function equalOrOperatorAfterNumber(visor, btnValue, operators) {
+  const lastIndex = visor.length - 1;
+
+  const hasOperator = visor.split("").some((char) => {
+    return operators.includes(char);
+  });
+  const equalPressedTooEarly = !hasOperator && btnValue === "=";
+  const pressedEqualAfterOperator =
+    operators.includes(visor[lastIndex]) && btnValue === "=";
+
+  if (equalPressedTooEarly || pressedEqualAfterOperator) {
     return true;
   }
   return false;
 }
 
 function doesSimpleMath(visor) {
-  const operators = ["+", "-", "*", "/"];
   let array = visor.split("");
 
-  let operatorIndex;
-
-  for (let operator of operators) {
-    operatorIndex = array.indexOf(operator);
-
-    if (operatorIndex !== -1) {
-      break;
-    }
-  }
+  const operatorIndex = getOperatorIndex(visor);
 
   let num1 = Number(array.slice(0, operatorIndex).join(""));
   let num2 = Number(array.slice(operatorIndex + 1).join(""));
@@ -62,26 +59,84 @@ function doesSimpleMath(visor) {
   }
 }
 
+function expressionHasOperator(visor) {
+  const operatorIndex = getOperatorIndex(visor);
+
+  if (operatorIndex === -1) {
+    return false;
+  }
+
+  return true;
+}
+
+function getOperatorIndex(visor) {
+  const operators = ["+", "-", "*", "/"];
+  let array = visor.split("");
+
+  for (let operator of operators) {
+    const index = array.indexOf(operator);
+
+    if (index !== -1) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+function addDotOrNot(visorValue, btnValue) {
+  if (btnValue !== ".") {
+    return false;
+  }
+
+  let operatorIndex = getOperatorIndex(visorValue);
+
+  if (operatorIndex === -1) {
+    let leftPart = visorValue;
+    const hasDotInLeft = leftPart.split("").some((char) => char === ".");
+    return hasDotInLeft;
+  } else {
+    let rightPart = visorValue.slice(operatorIndex + 1);
+    const hasDotInRight = rightPart.split("").some((char) => char === ".");
+    return hasDotInRight;
+  }
+}
+
 function startCalculator() {
   const visor = document.querySelector("#result");
   const calcButtons = document.querySelectorAll("button");
+  const operators = ["+", "-", "*", "/", "."];
 
   calcButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
       const btnValue = e.target.textContent;
 
-      if (operatorInFront(btnValue, visor.value)) {
+      if (addDotOrNot(visor.value, btnValue)) {
         return;
       }
 
-      if (!addOperatorOrNot(btnValue, visor.value)) {
+      if (operatorInFront(btnValue, visor.value, operators)) {
         return;
+      }
+
+      if (!addOperatorOrNot(btnValue, visor.value, operators)) {
+        return;
+      }
+
+      if (expressionHasOperator(visor.value)) {
+        const operators = ["+", "-", "*", "/"];
+        if (operators.includes(btnValue)) {
+          let calc = doesSimpleMath(visor.value);
+          visor.value = "";
+          visor.value += calc;
+        }
       }
 
       if (btnValue === "=") {
         if (visor.value === "") {
           return;
-        } else if (equalAfterNumber(visor.value)) {
+        } else if (
+          equalOrOperatorAfterNumber(visor.value, btnValue, operators)
+        ) {
           return;
         } else {
           let calc = doesSimpleMath(visor.value);
